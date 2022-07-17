d4c68dfe74fc10a0210e7c3b48f8548155f6e69e
Kurt_Nuske <nuske.kurt@gmail.com>
2022-07-18 08:36:00 +1000

    Js has started to be built

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,12 +1,32 @@
-var el = document.querySelector("#");
+
+if (selected == null){
+    //don't let submmit work
+    console.log('selected not set');
+}
+
+
+var one = document.getElementById('1');
+one.onclick = function() {
+    var selected = '1'
+    console.log(selected);
+}
+
+var two = document.getElementById('2');
+two.onclick = function() {
+    var selected = '2'
+    console.log(selected);
+}
 
 
 
-onclick set var as 12345
 
-if click changes reset var and choose new one
 
-unless var is set submit does nothing
-else var hides current html and displays Thank you message
 
-log rating to console
+//onclick set var as 12345
+
+//if click changes reset var and choose new one
+
+//unless var is set submit does nothing
+//else var hides current html and displays Thank you message
+
+//log rating to console
