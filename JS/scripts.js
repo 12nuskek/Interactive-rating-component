ffb2cb376300fc49d04638310a55ac48b61b20b2
Kurt_Nuske <nuske.kurt@gmail.com>
2022-07-18 08:08:16 +1000

    Updated rating hover buttons, JS logic written

---
+++ JS/scripts.js
@@ -0,0 +1,12 @@
+var el = document.querySelector("#");
+
+
+
+onclick set var as 12345
+
+if click changes reset var and choose new one
+
+unless var is set submit does nothing
+else var hides current html and displays Thank you message
+
+log rating to console
